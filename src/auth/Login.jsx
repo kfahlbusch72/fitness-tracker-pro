513cbe58c0edb43a8f517903aeeeeb5e70181ec8
karl <karl.fahlbusch7241@gmail.com>
2025-07-15 13:05:03 -0500

    login jsx update

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { useAuth } from "./AuthContext";
-import { usePage } from "../layout/PageContext";
+import { useNavigate } from "react-router-dom";
 
 /** A form that allows users to log into an existing account. */
 export default function Login() {
   const { login } = useAuth();
-  const { setPage } = usePage();
+  const navigate = useNavigate();
 
   const [error, setError] = useState(null);
 
@@ -14,7 +14,7 @@ export default function Login() {
     const password = formData.get("password");
     try {
       await login({ username, password });
-      setPage("activities");
+      navigate("/activities");
     } catch (e) {
       setError(e.message);
     }
@@ -35,7 +35,9 @@ export default function Login() {
         <button>Login</button>
         {error && <output>{error}</output>}
       </form>
-      <a onClick={() => setPage("register")}>Need an account? Register here.</a>
+      <a onClick={() => navigate("/register")}>
+        Need an account? Register here.
+      </a>
     </>
   );
 }
