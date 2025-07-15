888c2dcd91f59fe72f5d098f1f2282c6704bad0b
karl <karl.fahlbusch7241@gmail.com>
2025-07-15 12:40:59 -0500

    navbar jsx update

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,21 +1,27 @@
 import { useAuth } from "../auth/AuthContext";
-import { usePage } from "./PageContext";
+import { Link, useNavigate } from "react-router-dom";
 
 /** Navbar with site navigation links */
 export default function Navbar() {
   const { token, logout } = useAuth();
-  const { setPage } = usePage();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
   return (
     <header>
       <p>Fitness Trackr</p>
       <nav>
-        <a onClick={() => setPage("activities")}>Activities</a>
+        <Link to="/activities">Activities</Link>
         {token ? (
-          <a onClick={() => logout()}>Log out</a>
+          <a onClick={handleLogout}>Log out</a>
         ) : (
           <>
-            <a onClick={() => setPage("register")}>Register</a>
-            <a onClick={() => setPage("login")}>Login</a>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
           </>
         )}
       </nav>
